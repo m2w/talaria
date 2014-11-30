20a0d32dcd4ac5f205d6210deb76109eee475e6b
Moritz <moritz@tibidat.com>
2014-11-30 21:23:42 +0100

    Fixed the tests so they properly fail.

--- test/main.js
+++ test/main.js
@@ -170,6 +170,96 @@ var expect = chai.expect,
                 "updated_at": "2011-04-14T16:00:49Z"
             }
         ]
+    ),
+    issueMappingsResponse = JSON.stringify([]),
+    singleCommentIssueResponse = JSON.stringify(
+        [
+            {
+                "id": 1,
+                "url": "https://api.github.com/repos/octocat/Hello-World/issues/comments/1",
+                "html_url": "https://github.com/octocat/Hello-World/issues/1347#issuecomment-1",
+                "body": "Me too",
+                "user": {
+                    "login": "octocat",
+                    "id": 1,
+                    "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+                    "gravatar_id": "",
+                    "url": "https://api.github.com/users/octocat",
+                    "html_url": "https://github.com/octocat",
+                    "followers_url": "https://api.github.com/users/octocat/followers",
+                    "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+                    "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+                    "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+                    "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+                    "organizations_url": "https://api.github.com/users/octocat/orgs",
+                    "repos_url": "https://api.github.com/users/octocat/repos",
+                    "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+                    "received_events_url": "https://api.github.com/users/octocat/received_events",
+                    "type": "User",
+                    "site_admin": false
+                },
+                "created_at": "2011-04-14T16:00:49Z",
+                "updated_at": "2011-04-14T16:00:49Z"
+            }
+        ]
+    ),
+    multiCommentIssueResponse = JSON.stringify(
+        [
+            {
+                "id": 1,
+                "url": "https://api.github.com/repos/octocat/Hello-World/issues/comments/1",
+                "html_url": "https://github.com/octocat/Hello-World/issues/1347#issuecomment-1",
+                "body": "Me too",
+                "user": {
+                    "login": "octocat",
+                    "id": 1,
+                    "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+                    "gravatar_id": "",
+                    "url": "https://api.github.com/users/octocat",
+                    "html_url": "https://github.com/octocat",
+                    "followers_url": "https://api.github.com/users/octocat/followers",
+                    "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+                    "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+                    "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+                    "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+                    "organizations_url": "https://api.github.com/users/octocat/orgs",
+                    "repos_url": "https://api.github.com/users/octocat/repos",
+                    "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+                    "received_events_url": "https://api.github.com/users/octocat/received_events",
+                    "type": "User",
+                    "site_admin": false
+                },
+                "created_at": "2011-04-14T16:00:49Z",
+                "updated_at": "2011-04-14T16:00:49Z"
+            },
+            {
+                "id": 2,
+                "url": "https://api.github.com/repos/octocat/Hello-World/issues/comments/2",
+                "html_url": "https://github.com/octocat/Hello-World/issues/1347#issuecomment-2",
+                "body": "Numero Duo",
+                "user": {
+                    "login": "octocat",
+                    "id": 1,
+                    "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+                    "gravatar_id": "",
+                    "url": "https://api.github.com/users/octocat",
+                    "html_url": "https://github.com/octocat",
+                    "followers_url": "https://api.github.com/users/octocat/followers",
+                    "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+                    "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+                    "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+                    "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+                    "organizations_url": "https://api.github.com/users/octocat/orgs",
+                    "repos_url": "https://api.github.com/users/octocat/repos",
+                    "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+                    "received_events_url": "https://api.github.com/users/octocat/received_events",
+                    "type": "User",
+                    "site_admin": false
+                },
+                "created_at": "2011-04-14T17:00:49Z",
+                "updated_at": "2011-04-14T17:00:49Z"
+            }
+        ]
     );
 
 describe('talaria.GISTS', function () {
@@ -419,7 +509,9 @@ describe('talaria.ISSUES', function () {
     });
     afterEach(function () {
         var w = document.querySelector('div.talaria-wrapper');
-        w.parentNode.removeChild(w);
+        if (w !== null) {
+            w.parentNode.removeChild(w);
+        }
     });
     it('should display an error when no correct issue<=>permalink mappings can be found', function () {
         document.querySelector('a.permalink').setAttribute('href','/nonexistant/');
