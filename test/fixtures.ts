2e8df62ef812c451f9894072f5e53bd3d288db82
Moritz <oss@tibidat.com>
2017-08-21 21:20:16 +0200

    Update/improve test fixtures

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -14,24 +14,52 @@ export const comment = (id: number) => {
   };
 };
 
-export const comments = [comment(1), comment(2), comment(3), comment(4)];
-
-export const cachedComments: any = {
-  ts: new Date().getTime(),
-  value: comments
+export const urls = {
+  mappingsMissing: '/test-missing-mappings.json',
+  mappings: '/test-mappings.json',
+  content: '/github-api/url/',
+  contentCached: '/github-api/url/cached',
+  contentExpiredCache: '/github-api/url/expired-cache/',
+  contentMissing: '/github-api/url/missing',
+  permalink: '/test-permalink',
+  permalinkNoMapping: '/test-permalink/no-mapping',
+  permalinkInvalidMapping: '/test-permalink/wrong-mapping'
 };
+
+export const mappings = {};
+mappings[urls.permalink] = {
+  id: 'test-id'
+};
+mappings[urls.permalinkInvalidMapping] = {
+  id: 'test-id-missing'
+};
+
+export const jsonResp = (body) => {
+  return [
+    200,
+    { 'Content-Type': 'application/json' },
+    JSON.stringify(body)
+  ];
+}
+
+export const comments = [
+  comment(1),
+  comment(2),
+  comment(3),
+  comment(4)
+];
 
 export const bareTalariaConfig = {
   backend: Backend.Gists,
-  mappingUrl: '/my-mappings.json',
+  mappingUrl: urls.mappings,
   github_username: 'a-user',
   cacheTimeout: 60 * 60 * 1000
 };
 
-export const testContentId = "test-content"
+export const testContentId = 'test-content';
 
 export const testContent = (href) => {
-  return `<article id="${testContentId}">
+  return `<article id="${href}">
 <a class="permalink" href="${href}">Testing!</a>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
